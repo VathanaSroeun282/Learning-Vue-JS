7e8d6a762b00f00d92cb66adf3f25653337341c2
Vathana Sroeun <vathanasroeun@gmail.com>
2024-06-06 13:54:53 +0700

    myNote.txt was add

---
+++ my-project/src/components/BindingDataTOAttribute.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="w-full h-[100vh] bg-green-50">
+        <a :href="links">Click Here</a>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            links: "myLink.vue"
+        }
+    },
+}
+</script>
