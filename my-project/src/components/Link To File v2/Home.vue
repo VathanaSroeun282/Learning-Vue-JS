a63b070935880e4896c3299d48505941fb535930
VathanaSroeun282 <vathanasroeun@gmail.com>
2024-06-11 20:00:36 +0700

    Create methods for button Login and Sign UP

--- my-project/src/components/Link To File v2/Home.vue
+++ my-project/src/components/Link To File v2/Home.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="m-auto w-[50%] h-[20rem] bg-blue-500 border rounded-md  flex justify-center items-center gap-20">
-        <button class="w-36 h-20 font-bold text-2xl bg-green-500 rounded-md hover:bg-green-900 hover:text-white" type="button">Login</button>
-        <button class="w-36 h-20 font-bold text-2xl bg-yellow-500 rounded-md hover:bg-yellow-900 hover:text-white" type="button">Sign Up</button>
+        <button class="w-36 h-20 font-bold text-2xl bg-green-500 rounded-md hover:bg-green-900 hover:text-white" type="button" @click="goToLogin">Login</button>
+        <button class="w-36 h-20 font-bold text-2xl bg-yellow-500 rounded-md hover:bg-yellow-900 hover:text-white" type="button" @click="goToSignUp">Sign Up</button>
     </div>
 </template>
+<script>
+    export default {
+        methods: {
+            goToLogin() {
+                this.$router.push('/Login')
+            },
+            goToSignUp() {
+                this.$router.push('/SignUp')
+            },
+        },
+    }
+</script>
