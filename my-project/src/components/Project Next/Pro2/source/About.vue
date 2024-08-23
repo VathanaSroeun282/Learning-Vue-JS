1d2ab6308414aec50a65f4bd380959beb8cd5325
unknown <vathanasroeun@gmail.com>
2024-08-23 15:37:40 +0700

    GG

--- my-project/src/components/Project Next/Pro2/source/About.vue
+++ my-project/src/components/Project Next/Pro2/source/About.vue
@@ -9,8 +9,8 @@
             <p class="flex mb-2"><img class="h-6" :src=dataInfo.iconDes alt="icon3">{{ dataInfo.subDescription.sub3 }}</p>
             <button class="bg-blue-600 mt-4 w-36 h-10 text-white flex items-center rounded-md gap-2 relative group hover:bg-blue-700"><span class="ml-4">Read more</span> <img class="w-6 transition-all delay-75 absolute right-5 group-hover:right-2" :src=dataInfo.iconBtn alt="arrowright"></button>
         </div>
-        <div class="flex justify-between w-[36.5%]">
-            <div><img class="w-64 rounded-md" :src=dataInfo.img.img1 alt="img1"></div>
+        <div class="w-[36.5%] flex justify-between gap-5">
+            <div><img class="w-64" :src=dataInfo.img.img1 alt="img1"></div>
             <div class="flex flex-col justify-between">
                 <div><img class="w-64 rounded-md" :src=dataInfo.img.img2 alt="img2"></div>
                 <div><img class="w-64 rounded-md" :src=dataInfo.img.img3 alt="img3"></div>
